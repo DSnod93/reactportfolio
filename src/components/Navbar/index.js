04a4943591c978b2cdaadbbcf051d4bd4c06ad5d
DSnoddy <douglassnoddyjr@gmail.com>
2021-06-24 13:07:46 -0800

    added icons to nav

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,21 +1,35 @@
 import React, { Component } from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPortrait, faProjectDiagram, faFile, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 const MainNav = () => {
     return (
         <>
             <Navbar bg="dark" expand="lg">
-                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav" >
                     <Nav className="ml-auto nav-pill" >
-                        <Nav.Link className="text-white" href="#about">About</Nav.Link>
-                        <Nav.Link className="text-white" href="#projects">Projects</Nav.Link>
-                        <Nav.Link className="text-white" href="#resume">Resume</Nav.Link>
-                        <Nav.Link className="text-white" href="#contact">Contact</Nav.Link>
+                        <Nav.Link className="text-white" href="#about">
+                            <FontAwesomeIcon icon={faPortrait} /> &nbsp;
+                            About
+                        </Nav.Link>
+                        <Nav.Link className="text-white" href="#projects">
+                            <FontAwesomeIcon icon={faProjectDiagram} />  &nbsp;
+                            Projects
+                        </Nav.Link>
+                        <Nav.Link className="text-white" href="#resume">
+                            <FontAwesomeIcon icon={faFile} />  &nbsp;
+                            Resume
+                        </Nav.Link>
+                        <Nav.Link className="text-white" href="#contact">  
+                            <FontAwesomeIcon icon={faEnvelope} />  &nbsp;
+                            Contact
+                        </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
-            </Navbar>        
-            </>
+            </Navbar>
+        </>
     )
 }
 
