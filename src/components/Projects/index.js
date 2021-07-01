4d451af633bc30716ec0de03c50e7b75d06f3737
DSnoddy <douglassnoddyjr@gmail.com>
2021-06-30 20:56:49 -0800

    added second row of projects

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,9 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component, } from 'react';
 import { Container, Col, Row, Image, Button } from 'react-bootstrap';
 import project1 from '../images/cuisine-time.jpg';
 import project2 from '../images/food-blog.JPG';
 import project3 from '../images/foodchain.JPG';
-
+import work1 from '../images/notetaker.JPG';
+import work2 from '../images/run-buddy.JPG';
+import work3 from '../images/workday.JPG';
 
 const Projects = () => {
     return (
@@ -25,6 +27,24 @@ const Projects = () => {
                     <Image src={project3} width="350" height="320" />
                     <h3 className="mt-4">The Food Chain</h3>
                     <p>MongoDB, Express, React, Node, JWT, Stripe, Materialize, GraphQL, bcrypt, Mongoose</p>
+                    <Button className="m-4"><a></a>Click Here</Button>
+                </Col>
+                <Col md={true}>
+                    <Image className="mt-5" src={work1} width="350" height="320" />
+                    <h3 className="mt-4">Note Taker</h3>
+                    <p>Node, Express, Javascript, HTML, CSS</p>
+                    <Button className="m-4"><a></a>Click Here</Button>
+                </Col>
+                <Col md={true}>
+                    <Image className="mt-5" src={work2} width="350" height="320" />
+                    <h3 className="mt-4">Run-Buddy</h3>
+                    <p>HTML, CSS</p>
+                    <Button className="m-4"><a></a>Click Here</Button>
+                </Col>
+                <Col md={true}>
+                    <Image className="mt-5" src={work3} width="350" height="320" />
+                    <h3 className="mt-4">Work Day Scheduler</h3>
+                    <p>HTML, CSS, Bootstrap, jQuery, Moment.js</p>
                     <Button className="m-4"><a></a>Click Here</Button>
                 </Col>
             </Row>
